07d5674b7c03a3ecf2dccac717c293f631921c9c
changoo <changoo@Changoo-PC>
2016-10-10 19:27:33 +0900

    after

--- src/code2_10/Main.java
+++ src/code2_10/Main.java
@@ -3,11 +3,11 @@ package code2_10;
 public class Main {
 	
 	public static void main(String[] args){
-			MyStack<String> st = new MyStack<String>();
+			MyStack st = new MyStack();
 			st.push("changoo1");
 			st.push("changoo2");
 			System.out.println(st.pop());
-			st.set(0,"changoo3"); 
+			st.push("changoo3");
 			System.out.println(st.pop());
 	}		
 
--- src/code2_10/MyStack.java
+++ src/code2_10/MyStack.java
@@ -2,14 +2,15 @@ package code2_10;
 
 import java.util.ArrayList;
 
-public class MyStack<String> extends ArrayList<String> {
+public class MyStack{
+	ArrayList <String> array=new ArrayList<String>();
 	
 	public void push(String element){
-		add(element);
+		array.add(element);
 	}
 	
 	public String pop(){
-		return remove(size()-1);
+		return array.remove(array.size()-1);
 	}
 
 }
